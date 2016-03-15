0873d1e874127682070f2d67923c6f9a22305521
Joost Ouwerling <joost.ouwerling@mobiyou.nl>
2016-03-15 12:47:09 +0100

    Implemented MatchDAO

---
+++ project/webservice/Webservice/src/main/java/com/sportperformancemanagement/webservice/dao/MatchDAO.java
@@ -0,0 +1,52 @@
+package com.sportperformancemanagement.webservice.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.sportperformancemanagement.common.Match;
+import com.sportperformancemanagement.common.MySQLConnection;
+
+public class MatchDAO {
+	
+	/**
+	 * Used for logging
+	 */
+	private static final Logger logger =
+	        Logger.getLogger(MatchDAO.class.getName());
+	
+	/**
+	 * Get a list of all the matches from the database.
+	 * @return an array of Match objects
+	 * @throws Exception if there was an error loading the matches from the database.
+	 */
+	public Match[] getMatches() throws Exception {
+		try {
+			Connection conn = MySQLConnection.instance();
+			Statement stmt = conn.createStatement();
+			String query = "SELECT id, name, server, port FROM matches";
+			ResultSet rs = stmt.executeQuery(query);
+			List<Match> matches = new ArrayList<Match>();
+			while (rs.next()) {
+				matches.add(new Match(
+						rs.getString("name"),
+						rs.getInt("id"),
+						rs.getString("server"),
+						rs.getInt("port")));
+			}
+			logger.log(Level.INFO, matches.size() + " matches have been loaded from the database");
+			Match[] matchesArr = new Match[matches.size()];
+			matchesArr = matches.toArray(matchesArr);
+			return matchesArr;
+		} catch (SQLException ex) {
+			logger.log(Level.SEVERE, "Could not load Matches from database", ex);
+			throw new Exception("Could not load matches from database. See the logs for more details.");
+		}
+	}
+	
+}
--- project/webservice/Webservice/src/main/java/com/sportperformancemanagement/webservice/server/MatchesResource.java
+++ project/webservice/Webservice/src/main/java/com/sportperformancemanagement/webservice/server/MatchesResource.java
@@ -1,27 +1,46 @@
 package com.sportperformancemanagement.webservice.server;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.sportperformancemanagement.common.Match;
 import com.sportperformancemanagement.common.MatchJSON;
+import com.sportperformancemanagement.webservice.dao.MatchDAO;
 
 @Path("/")
 public class MatchesResource {
 	
-	public static Match[] matches;
+	/**
+	 * Used for logging
+	 */
+	private static final Logger logger =
+	        Logger.getLogger(MatchesResource.class.getName());
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/matches")
 	public String getMatches() {
-		MatchesResource.fillMatches();
-		return MatchJSON.matchesToJson(matches).toString();
+		MatchDAO matchDao = new MatchDAO();
+		try {
+			Match[] matches = matchDao.getMatches();
+			logger.log(Level.INFO, "Received " + matches.length + " matches from matchDAO");
+			return MatchJSON.matchesToJson(matches).toString();
+		} catch (Exception ex) {
+			logger.log(Level.SEVERE, "Error in loading matches from database", ex);
+			throw new WebApplicationException(
+					Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build());
+		}
 	}
 	
 	@GET
@@ -38,15 +57,6 @@ public class MatchesResource {
 			arr.put(obj);
 		}
 		return arr.toString();
-	}
-	
-	public static void fillMatches() {
-		if (matches != null)
-			return;
-		int size = 100;
-		matches = new Match[size];
-		for (int i = 0; i < size; i++)
-			matches[i] = new Match("Match " + i, i, "webserver.com", i + 100);
 	}
 	
 }
