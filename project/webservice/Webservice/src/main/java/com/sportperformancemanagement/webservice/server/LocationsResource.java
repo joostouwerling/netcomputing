44eba160f43d1b234682763b26e5cb434f94e786
Joost Ouwerling <joost.ouwerling@mobiyou.nl>
2016-03-16 00:16:04 +0100

    Implemented LocationsResource

---
+++ project/webservice/Webservice/src/main/java/com/sportperformancemanagement/webservice/server/LocationsResource.java
@@ -0,0 +1,107 @@
+package com.sportperformancemanagement.webservice.server;
+
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("/locations")
+public class LocationsResource {
+	
+	/**
+	 * Used for logging
+	 */
+	private static final Logger logger =
+	        Logger.getLogger(LocationsResource.class.getName());
+	
+	private static HashMap<Integer, Integer> countIncomingLocations = new HashMap<Integer, Integer>();
+	private static HashMap<Integer, String> locationsJson = new HashMap<Integer, String>();
+	private static int key = 0;
+	
+	@GET
+	@Path("/{match}/{player}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void asyncGetWithTimeout(@Suspended final AsyncResponse asyncResponse,
+									@PathParam("match") final int matchId,
+									@PathParam("player") final int playerId) {
+	    /*asyncResponse.setTimeoutHandler(new TimeoutHandler() {
+	 
+	        @Override
+	        public void handleTimeout(AsyncResponse asyncResponse) {
+	            asyncResponse.resume(Response.status(Response.Status.SERVICE_UNAVAILABLE)
+	                    .entity("Operation time out.").build());
+	        }
+	    });
+	    asyncResponse.setTimeout(10, TimeUnit.SECONDS);*/
+	 
+	    new Thread(new Runnable() {
+	 
+	        @Override
+	        public void run() {
+	            String result = requestLocationsFromDataServers();
+	            asyncResponse.resume(result);
+	        }
+	 
+	        private String requestLocationsFromDataServers() {
+	        	try {
+	        		int current_key = key;
+	        		key++;
+	        		countIncomingLocations.put(current_key, 0);
+	        		locationsJson.put(current_key, "[]");
+	        		logger.log(Level.INFO, "Started listening for locations on key " + current_key);
+	        		// notify dataservers via rabbit mq
+	        		Thread.sleep(15000);
+	        		String output = locationsJson.get(current_key);
+	        		logger.log(Level.INFO, "Received locations from " + countIncomingLocations.get(current_key) + " dataservers.");
+	        		logger.log(Level.INFO, "Data received: " + output);
+	        		countIncomingLocations.remove(current_key);
+	        		locationsJson.remove(current_key);
+	        		return output;
+	        	} catch (Exception ex) {
+	        		logger.log(Level.SEVERE, "Error executing request for locations", ex);
+	        		throw new WebApplicationException(
+	    					Response.status(Status.INTERNAL_SERVER_ERROR).entity("Could not load the locations.").build());
+	        	}
+	        }
+	    }).start();
+	}
+	
+	@POST
+	@Path("/{key}")
+	public void handleIncomingLocations(@PathParam("key") int key, @FormParam("locations") String locations) {
+		if (!countIncomingLocations.containsKey(key)) {
+			logger.log(Level.INFO, "Key " + key + "received but not found in the Hash Map.");
+			throw new WebApplicationException(
+					Response.status(Status.NOT_FOUND).entity("Key " + key + " not found in the system.").build());
+		}
+		if (locations == null) {
+			logger.log(Level.INFO, "Received a post request for " + key + " but no locations were submitted.");
+			throw new WebApplicationException(
+					Response.status(Status.BAD_REQUEST).entity("No locations were submitted in the post request").build());
+		}
+		logger.log(Level.INFO, "Received the following JSON for key " + key + ": " + locations);
+		countIncomingLocations.put(key, countIncomingLocations.get(key) + 1);
+		String jsonNow = locationsJson.get(key);
+		String newJson;
+		if (jsonNow == "[]")
+			newJson = locations;
+		else
+			newJson = jsonNow.substring(0, jsonNow.length() - 1) + "," + locations.substring(1);
+		locationsJson.put(key, newJson);
+	}
+	
+
+	
+}
--- project/webservice/Webservice/src/main/java/com/sportperformancemanagement/webservice/server/MatchesResource.java
+++ project/webservice/Webservice/src/main/java/com/sportperformancemanagement/webservice/server/MatchesResource.java
@@ -11,14 +11,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import com.sportperformancemanagement.common.Match;
 import com.sportperformancemanagement.common.MatchJSON;
 import com.sportperformancemanagement.webservice.dao.MatchDAO;
 
-@Path("/")
+@Path("/matches")
 public class MatchesResource {
 	
 	/**
@@ -29,7 +26,6 @@ public class MatchesResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/matches")
 	public String getMatches() {
 		MatchDAO matchDao = new MatchDAO();
 		try {
@@ -42,21 +38,5 @@ public class MatchesResource {
 					Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build());
 		}
 	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/locations")
-	public String getLocations() {
-		double lat = 53.2403851;
-		double lng = 6.5365328;
-		JSONArray arr = new JSONArray();
-		for (double i = -50; i < 50; i++) {
-			JSONObject obj = new JSONObject();
-			obj.put("lat", lat + (i / 1000));
-			obj.put("lng", lng + (i / 1000));
-			arr.put(obj);
-		}
-		return arr.toString();
-	}
-	
+		
 }
