f37efcb65a400aaf1550d3af5de8e996e9235268
Joost Ouwerling <joost.ouwerling@mobiyou.nl>
2016-03-22 10:22:03 +0100

    Documented PlayerResource and MatchResource. All webservice is now documented

--- webservice/Webservice/src/main/java/eu/sportperformancemanagement/webservice/server/MatchesResource.java
+++ webservice/Webservice/src/main/java/eu/sportperformancemanagement/webservice/server/MatchesResource.java
@@ -18,6 +18,12 @@ import eu.sportperformancemanagement.common.MatchJSON;
 import eu.sportperformancemanagement.webservice.dao.AlreadyExistsException;
 import eu.sportperformancemanagement.webservice.dao.MatchDAO;
 
+/**
+ * This class acts as the REST endpoints for fetching matches and
+ * inserting new matches.
+ * @author Joost Ouwerling <j.t.ouwerling@student.rug.nl>
+ *
+ */
 @Path("/matches")
 public class MatchesResource {
 	
@@ -27,16 +33,30 @@ public class MatchesResource {
 	private static final Logger logger =
 	        Logger.getLogger(MatchesResource.class.getName());
 	
+	
+	/**
+	 * The match data access object
+	 */
 	private MatchDAO matchDao;
 	
+	/**
+	 * Initialize matchDAO
+	 */
 	public MatchesResource() {
 		matchDao = new MatchDAO();
 	}
 	
 	
+	/**
+	 * Listen for GET requests for all matches. If it can sucessfully be retrieved
+	 * from the database, return it as a JSON array.
+	 * @return a JSON array with all matches from the database
+	 * @throws WebApplicationException if anything fails in the db process (500
+	 * Internal Server Error).
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getMatches() {
+	public String getMatches() throws WebApplicationException {
 		try {
 			Match[] matches = matchDao.getAll();
 			logger.log(Level.INFO, "Received " + matches.length + " matches from matchDAO");
@@ -48,10 +68,20 @@ public class MatchesResource {
 		}
 	}
 	
+	/**
+	 * Listen for POST requests to create a new match. 
+	 * @param port the port of the match server
+	 * @param name the name of the match
+	 * @param server the hostname of the match server
+	 * @return a CREATED response if everything was sucessfull
+	 * @throws WebApplicationException if a) the input was errornous (400 Bad Request)
+	 * b) the match already exists in the db with this name (Status.Conflict)
+	 * c) an error while processing your request (500 internal server error)
+	 */
 	@POST
 	public Response insertMatch(@FormParam("port") int port,
 							@FormParam("name") String name,
-							@FormParam("server") String server) {
+							@FormParam("server") String server) throws WebApplicationException {
 		Match match;
 		
 		/**
@@ -66,16 +96,21 @@ public class MatchesResource {
 					Response.status(Status.BAD_REQUEST).entity(ex.getMessage()).build());	
 		}
 		
+		// Try to insert the match in the database
 		try {
 			matchDao.insert(match);
+		// The match already exists
 		} catch (AlreadyExistsException ex) {
 			throw new WebApplicationException(
 					Response.status(Status.CONFLICT).entity(ex.getMessage()).build());	
+		// A server error.
 		} catch (Exception ex) {
 			logger.log(Level.WARNING, "Match could not be inserted.", ex);
 			throw new WebApplicationException(
 					Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build());	
 		}
+		
+		// Return the created response
 		return Response.status(Status.CREATED).entity(MatchJSON.matchToJson(match).toString()).build();
 	}
 	
--- webservice/Webservice/src/main/java/eu/sportperformancemanagement/webservice/server/PlayersResource.java
+++ webservice/Webservice/src/main/java/eu/sportperformancemanagement/webservice/server/PlayersResource.java
@@ -18,6 +18,13 @@ import eu.sportperformancemanagement.common.PlayerJSON;
 import eu.sportperformancemanagement.webservice.dao.AlreadyExistsException;
 import eu.sportperformancemanagement.webservice.dao.PlayerDAO;
 
+/**
+ * This class acts as REST endpoint for fetching all players
+ * and inserting new players.
+ * 
+ * @author Joost Ouwerling <j.t.ouwerling@student.rug.nl>
+ *
+ */
 @Path("/players")
 public class PlayersResource {
 	
@@ -27,16 +34,28 @@ public class PlayersResource {
 	private static final Logger logger =
 	        Logger.getLogger(MatchesResource.class.getName());
 	
+	/**
+	 * The database access object for players.
+	 */
 	private PlayerDAO playerDao;
 	
+	/**
+	 * Initialize the database access object
+	 */
 	public PlayersResource() {
 		playerDao = new PlayerDAO();
 	}
 	
 	
+	/**
+	 * GET a JSON list of all players. When the database fails to return
+	 * a list of players, a 500 internal server error is thrown.
+	 * @return a list of all players in JSON format
+	 * @throws WebApplicationException if something went wrong in the db
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getPlayers() {
+	public String getPlayers() throws WebApplicationException {
 		try {
 			Player[] players = playerDao.getAll();
 			logger.log(Level.INFO, "Received " + players.length + " players from playerDAO");
@@ -48,8 +67,23 @@ public class PlayersResource {
 		}
 	}
 	
+	/**
+	 * This method listens for POST requests, which want to insert a new
+	 * player in the database. It first tries to create a player object,
+	 * which might throw if name is invalid. Then it tries to insert the 
+	 * new player in the database
+	 * 
+	 * @param name the name of the new player
+	 * @return a CREATED response if everything was succesfull
+	 * @throws WebApplicationException if a) the input was invalid (400 bad request)
+	 * or b) if there was an error while inserting the player in the db
+	 * (500 internal server error) or c) when a player with this name already exists
+	 * (Status.CONFLICT).
+	 */
 	@POST
-	public Response insertPlayer(@FormParam("name") String name) {
+	public Response insertPlayer(@FormParam("name") String name) throws WebApplicationException {
+		
+		// Try to create the player object without id, so error checking is done
 		Player player;
 		try {
 			player = new Player(name); 
@@ -59,16 +93,21 @@ public class PlayersResource {
 					Response.status(Status.BAD_REQUEST).entity(ex.getMessage()).build());	
 		}
 		
+		// Insert it in the database
 		try {
 			playerDao.insert(player);
+		// The player already exists!
 		} catch (AlreadyExistsException ex) {
 			throw new WebApplicationException(
 					Response.status(Status.CONFLICT).entity(ex.getMessage()).build());	
+		// Another error while inserting it in the database
 		} catch (Exception ex) {
 			logger.log(Level.WARNING, "Player could not be inserted.", ex);
 			throw new WebApplicationException(
 					Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build());	
 		}
+		
+		// Return the created resonse.
 		return Response.status(Status.CREATED).entity(PlayerJSON.playerToJson(player).toString()).build();
 	}
 
